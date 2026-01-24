0b6f795ba84a0705a7c038d4a6a49c437dc4080e
Pankaj Kumar Gupta <140154353+pankajkrg@users.noreply.github.com>
2026-01-24 23:27:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,41 +4,6 @@
 provider "aws" {
   region = "ap-south-1"  # Change to your AWS region
 }
-
-# -------------------------------
-# S3 BUCKET
-# -------------------------------
-resource "aws_s3_bucket" "example" {
-  bucket = "my-unique-bucket-12345"  # Must be globally unique
-  acl    = "private"
-}
-
-# -------------------------------
-# SECURITY GROUP
-# -------------------------------
-resource "aws_security_group" "web_sg" {
-  name        = "web-sg"
-  description = "Allow SSH and HTTP"
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # SSH access from anywhere
-  }
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # HTTP access
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 # -------------------------------
 # EC2 INSTANCE
 # -------------------------------
@@ -52,14 +17,4 @@ resource "aws_instance" "web" {
   }
 }
 
-# -------------------------------
-# OUTPUTS
-# -------------------------------
-output "s3_bucket_name" {
-  value = aws_s3_bucket.example.bucket
-}
-
-output "ec2_instance_public_ip" {
-  value = aws_instance.web.public_ip
-}
 
